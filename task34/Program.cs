7c7a9ba7092323590e12e63c5763cc2a7e20e49c
Mikhail <Mikhey85@mail.ru>
2023-06-15 21:25:44 +0300

    add task 34

--- task34/Program.cs
+++ task34/Program.cs
@@ -4,10 +4,39 @@
 
 Console.Clear();
 
-int Promt (string mesage)
+
+int[] GetArray ()
 {
-    Console.Write(mesage);
-    int value = int.Parse(Console.ReadLine()!);
-    return value;
+    int[] Array = new int [new Random().Next(2,10)];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i]= new Random().Next(100,1000) ;
+    }
+     return (Array);
+ }
+
+int CountArray (int[] Array)
+{
+    int CountArray=0;
+    foreach (var item in Array)
+    {
+        if (item%2 == 0)
+        {
+            CountArray++;
+        }
+    }  
+     return (CountArray);
+ }
+
+
+void PrintArray (int[] Array) 
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i]+", ");
+    }
 }
-int[] Array ()
+
+int[] Array = GetArray();
+PrintArray(Array);
+Console.Write($" Кол-во четных чисел в массиве = {CountArray(Array)}");
